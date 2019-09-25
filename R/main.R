ce6ee0c81dca0a94db46050087fdd5b04214819a
Sayani07 <Sayani.Gupta@monash.edu>
2019-09-25 10:26:59 +1000

    adding rough structure

--- R/main.R
+++ R/main.R
@@ -5,3 +5,10 @@ library(sugrrants)
 library(tsibble)
 library(gravitas)
 
+smart_meter50 <- read_rds("~/Documents/paper-gravitas/data/sm_cust50.rds") %>%  
+  select(customer_id, 
+         reading_datetime,
+         general_supply_kwh, 
+         everything())
+
+smart_meter50
