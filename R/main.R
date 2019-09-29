e4ce0951a16d28a6be5e50d3fb11e1cb7dcea76d
Sayani07 <Sayani.Gupta@monash.edu>
2019-09-29 11:50:21 +1000

    gitignore gif and data

--- R/main.R
+++ R/main.R
@@ -6,16 +6,78 @@ library(tsibble)
 library(gravitas)
 library(kableExtra)
 library(gganimate)
+library(lubridate)
+library(ggridges)
+library(ggpubr)
 #remotes::install_github("njtierney/palap")
 library(palap)
 
-smart_meter50 <- read_rds("~/Documents/paper-gravitas/data/sm_cust50.rds") %>%  
+smart_meter50 <- read_rds("~/Documents/paper-gravitas/data/sm_cust50.rds")%>%  
   select(customer_id, 
          reading_datetime,
          general_supply_kwh, 
          everything())
 
+
+
+##----motivation1
+
+smart_meter50 %>% filter(customer_id %in% 10006414) %>% ggplot() + geom_line(aes(x =reading_datetime, y = general_supply_kwh), color = "#1B9E77")+ theme(legend.position = "bottom")
+
+
+##----motivation2
+smart_meter50 %>% filter(customer_id %in% c(10018254)) %>%  ggplot() + geom_line(aes(x =reading_datetime, y = general_supply_kwh, color = customer_id), color = "#D95F02") + theme(legend.position = "bottom")
+
+##----motivation3
+
+# smart_p <- smart_meter50 %>%  ggplot() + geom_line(aes(x =reading_datetime, y = general_supply_kwh, color = customer_id))  + theme(legend.position = "None")
+# 
+# smart_anim <-  smart_p + gganimate::transition_states(customer_id)+ labs(title = "{closest_state}")
+# 
+# gganimate::animate(smart_anim, fps = 10, width = 1000, height = 600)
+# 
+# 
+# anim_save("images/smart_allcust.gif")
+
+knitr::include_graphics("images/smart_allcust.gif")
+
+
+##----motivation5
+smart_meter50 %>%
+  filter(customer_id %in% 10006414) %>%
+  mutate(hour_day = hour(reading_datetime)) %>% 
+  ggplot() + geom_point(aes(x = hour_day, y = general_supply_kwh)) + 
+  theme(legend.position = "bottom") + ggtitle("10006414")
+
+
+##----motivation6
+
+# smart_p_period <- smart_meter50 %>%
+#   mutate(hour_day = hour(reading_datetime)) %>% 
+#   ggplot() + geom_point(aes(x = hour_day, y = general_supply_kwh, color = customer_id))  +  theme(legend.position = "None")
+# 
+# 
+# 
+# smart_anim_period <-  smart_p_period + gganimate::transition_states(customer_id)+ labs(title = "{closest_state}")
+# 
+# gganimate::animate(smart_anim_period, fps = 10, width = 1000, height = 600)
+# 
+# 
+# anim_save("images/smart_allcust_period.gif")
+
+knitr::include_graphics("images/smart_allcust_period.gif")
+
+
+##----motivation7
+# smart_meter50  %>%
+#   mutate(hour_day = hour(reading_datetime)) %>% 
+#   ggplot() + geom_point(aes(x = hour_day, y = general_supply_kwh, color = customer_id)) +  theme(legend.position = "none")
+
+# knitr::include_graphics("images/hd_allcust.png")
+
+##----read
 smart_meter50
+
 
 ##----search_gran
 
@@ -24,12 +86,10 @@ smart_meter50 %>%
   dynamic_search_gran(lowest_unit = "hhour", highest_unit =  "month")
 
 
-
 ##----create_gran
 
-
 smart_meter50 %>%
-  dynamic_create_gran("hour_week") %>% select(customer_id, 
+  create_gran("hour_week") %>% select(customer_id, 
                                               reading_datetime,
                                               hour_week,
                                               general_supply_kwh, 
@@ -41,6 +101,11 @@ smart_meter50 %>%
 smart_meter50 %>% is.harmony("hour_week","day_fortnight")
 
 ##----harmony
+cust1 <- smart_meter50 %>% distinct(customer_id) %>% head(10)
+cust2 <- smart_meter50 %>% distinct(customer_id) %>% head(20) %>% filter(!(customer_id %in% cust1))
+
+cust3 <- smart_meter50 %>% distinct(customer_id) %>% head(30)%>% filter(!(customer_id %in% c(cust1, cust2)))
+cust4 <- smart_meter50 %>% distinct(customer_id) %>% head(10)
 
 
 smart_meter50 %>% 
@@ -48,17 +113,77 @@ smart_meter50 %>%
           lgran = "hhour",
           filter_out = c("fortnight", "hhour"))
 
-##----granplotoverlay
+##----granplotoverlay1
+
+
 smart_meter50   %>% 
+  filter(customer_id %in% cust1$customer_id) %>% 
   granplot("month_year", 
            "hour_day",
            plot_type = "quantile", 
            response = "general_supply_kwh",
-           quantile_prob = c(0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99),
+           quantile_prob = c(0.25, 0.5, 0.75),
            overlay = TRUE) + ggtitle("") +
   scale_x_discrete(breaks = seq(0,23,2)) + 
   theme_remark()
 
+
+##----granplotoverlay2
+smart_meter50   %>% 
+  filter(customer_id %in% cust1$customer_id) %>% 
+  granplot("month_year", 
+           "hour_day",
+           plot_type = "quantile", 
+           response = "general_supply_kwh",
+           quantile_prob = c(0.1, 0.25, 0.5, 0.75, 0.9),
+           overlay = TRUE) + ggtitle("") +
+  scale_x_discrete(breaks = seq(0,23,2)) + 
+  theme_remark()
+
+##----granplotoverlay3
+smart_meter50   %>% 
+  filter(customer_id %in% cust1$customer_id) %>% 
+  granplot("month_year", 
+           "hour_day",
+           plot_type = "quantile", 
+           response = "general_supply_kwh",
+           quantile_prob = c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95),
+           overlay = TRUE) + ggtitle("") +
+  scale_x_discrete(breaks = seq(0,23,2)) + 
+  theme_remark()
+
+
+
+##----granplotoutlier
+
+# smart_meter50   %>% 
+#   filter(customer_id %in% cust1$customer_id) %>% 
+#   granplot("day_week", 
+#            "day_month",
+#            plot_type = "violin", 
+#            response = "general_supply_kwh",
+#            quantile_prob = c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95),
+#            overlay = TRUE) + ggtitle("") + 
+#   theme_remark()
+
+
+# smart_meter50  %>% create_gran("day_month") %>% filter(day_month %in% c(1:12)) %>% 
+#   granplot("day_week", 
+#            "day_month",
+#            plot_type = "violin", 
+#            response = "general_supply_kwh",
+#            quantile_prob = c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95),
+#            overlay = TRUE) + ggtitle("") + 
+#   theme_remark() + geom_boxplot(width = 0.01, color =" Blue")
+# 
+# smart_meter50  %>% create_gran("hour_day") %>% filter(hour_day %in% c(0:12)) %>% 
+#   granplot("week_month", 
+#            "hour_day",
+#            plot_type = "violin", 
+#            response = "general_supply_kwh",
+#            quantile_prob = c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95),
+#            overlay = TRUE) + ggtitle("") + 
+#   theme_remark() + geom_boxplot(width = 0.01, color =" Blue")
 
 
 
@@ -90,7 +215,7 @@ knitr::include_graphics("images/palap_quantile.gif")
 ##----cricket
 
 
-cricket <- read_rds("~/Documents/paper-gravitas/data/cricket_tsibble.rds") %>%  
+cricket <- read_rds("~/Documents/paper-gravitas/data/cricket_tsibble.rds")%>%  
   select(season, 
          match_id,
          inning,
@@ -146,4 +271,88 @@ glimpse(cricket)
 knitr::include_graphics("images/cricketex.gif")
 
 
-  
+##----lineartime
+
+knitr::include_graphics("images/linear-time.png")
+
+
+##----circulartime
+knitr::include_graphics("images/circular.png")
+
+
+##----calendar
+knitr::include_graphics("images/calendar.png")
+
+
+##----box
+
+mpg <- mpg %>% filter (class %in% c("compact", "midsize", "suv","minivan", "pickup")) %>% 
+                   mutate(cls = 
+                        case_when(
+                          class == "compact" ~ "A",
+                          class == "midsize" ~ "B",
+                          class == "suv" ~ "C",
+                          class == "minivan"  ~ "D",
+                          class == "pickup" ~"E"))
+                          
+                          
+
+p <- ggplot(mpg, aes(cls, hwy))
+
+
+p1 = p + geom_boxplot(fill = "white", colour = "#3366FF")+ xlab("") + ylab("")
+p1 
+
+##----violin
+p2 = p + geom_violin(fill = "white", colour = "#3366FF")+  xlab("") + ylab("")
+p2 
+##----lv
+p3 = p + geom_lv(aes(fill = ..LV..), outlier.colour = "red", outlier.shape = 1)+  xlab("") + ylab("")  + theme(legend.position = "None")
+p3 
+
+##----quantile
+p4_quantile <- mpg %>% group_by(cls) %>%  do({
+  x <- .$hwy
+  map_dfr(
+    .x = seq(0.1, 0.9, 0.1),
+    .f = ~ tibble(
+      Quantile = .x,
+      Value = quantile(x, probs = .x, na.rm = TRUE)
+    )
+  )
+})
+
+p4 <- p4_quantile %>% ggplot(aes(x = cls, y = Value, group=Quantile,  col = as.factor(Quantile))) + geom_line() +   xlab("") + ylab("") + theme(legend.position = "None") + scale_color_brewer(palette = "Dark2")
+
+p4 
+
+#----ridge
+p5 <- ggplot(mpg, aes( hwy, cls)) + geom_density_ridges2()+  xlab("") + ylab("")
+p5 
+
+#----overlay
+
+# p4_overlay <- mpg %>% group_by(cls) %>%  do({
+#   x <- .$hwy
+#   map_dfr(
+#     .x = c(0.1, 0.25, 0.5, 0.75, 0.9),
+#     .f = ~ tibble(
+#       Quantile = .x,
+#       Value = quantile(x, probs = .x, na.rm = TRUE)
+#     )
+#   )
+# })
+# 
+# 
+# p4_spread <- p4_overlay %>% spread(Quantile, Value)
+# 
+# ggplot(p4_spread) %>% geom_line(x = cls, y='0.5')
+
+##---types
+
+#ggarrange(p1, p2, p3, p4, p5, nrow = 2, ncol =3, labels = c("box", "violin", "lv", "decile", "ridge"))
+
+#----clash
+
+knitr::include_graphics("images/clash.png")
+
