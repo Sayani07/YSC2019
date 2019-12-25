1f5d13b1c8b6e6050dd6ef9350c6c6a6153e12d0
Sayani07 <Sayani.Gupta@monash.edu>
2019-12-25 11:44:52 +0530

    data structure page update

--- R/main.R
+++ R/main.R
@@ -10,6 +10,7 @@ library(lubridate)
 library(ggridges)
 library(ggpubr)
 library(lvplot)
+library(kableExtra)
 #remotes::install_github("njtierney/palap")
 library(palap)
 
@@ -82,9 +83,10 @@ smart_meter50
 
 
 search_gran <- smart_meter50 %>%
-  search_gran(lowest_unit = "hhour", highest_unit =  "month")
+  search_gran(lowest_unit = "hhour", highest_unit =  "month", 
+              filter_out = c("fortnight", "hhour"))
 
-knitr::kable(search_gran, row.names = TRUE)
+knitr::kable(search_gran, row.names = TRUE) %>% kable_styling(font_size = 14)
 
 
 ##----create_gran
@@ -112,7 +114,7 @@ cust4 <- smart_meter50 %>% distinct(customer_id) %>% head(10)
 smart_meter50 %>% 
   harmony(ugran = "month",
           lgran = "hhour",
-          filter_out = c("fortnight", "hhour")) %>% knitr::kable(row.names = TRUE)
+          filter_out = c("fortnight", "hhour")) %>% knitr::kable(row.names = TRUE) %>% kable_styling(font_size = 14)
 
 ##----granplotoverlay1
 
